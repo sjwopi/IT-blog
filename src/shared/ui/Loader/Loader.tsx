ba52326f4b31754cef9f9808376a352f6ed9462d
Ivan Teteruk <iteterk@gmail.com>
2023-05-23 09:42:35 -0200

    add loader

---
+++ src/shared/ui/Loader/Loader.tsx
@@ -0,0 +1,17 @@
+import { classNames } from 'shared/lib/classNames/classNames';
+import cls from './Loader.module.scss';
+
+interface LoaderProps {
+  className?: string;
+}
+
+export const Loader = ({ className }: LoaderProps) => {
+  return (
+    <div className={classNames(cls.ldsRipple, {}, [className])}>
+      <div></div>
+      <div></div>
+    </div>
+  );
+};
+
+export default Loader;
